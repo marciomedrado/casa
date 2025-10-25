a7ef21383cb90035c814a87f22a32642e1f38461
Márcio Medrado <marciomedrado@gmail.com>
2025-10-25 15:09:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5853692942-d4adf",
+  "appId": "1:698835905071:web:3100f1c06d1ce0556cf4da",
+  "apiKey": "AIzaSyAPCV8Ps5BzuvAULZf5Oaxftr2BJwc0Xqw",
+  "authDomain": "studio-5853692942-d4adf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "698835905071"
+};
